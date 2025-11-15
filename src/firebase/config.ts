6eacc4da991a563002407ac11a262e47dab6d5fc
Jerico Ada <mephistojeric.mcmxcii@gmail.com>
2025-11-15 05:56:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5617832182-dbc8f",
+  "appId": "1:253491721710:web:2d35df6ceb47089584caff",
+  "apiKey": "AIzaSyDS-DPkb1jgxNbWbzQThRMWVO8rvbevsdM",
+  "authDomain": "studio-5617832182-dbc8f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "253491721710"
+};
