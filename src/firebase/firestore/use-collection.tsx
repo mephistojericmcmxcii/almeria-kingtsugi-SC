cbb56fb1f099ef1254221b08c9be25b77bc22b1e
Jerico Ada <mephistojeric.mcmxcii@gmail.com>
2025-11-15 10:34:01 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -109,7 +109,10 @@ export function useCollection<T = any>(
     return () => unsubscribe();
   }, [memoizedTargetRefOrQuery]); // Re-run if the target query/reference changes.
   if(memoizedTargetRefOrQuery && !memoizedTargetRefOrQuery.__memo) {
-    throw new Error(memoizedTargetRefOrQuery + ' was not properly memoized using useMemoFirebase');
+    const queryPath = memoizedTargetRefOrQuery.type === 'collection'
+      ? (memoizedTargetRefOrQuery as CollectionReference).path
+      : (memoizedTargetRefOrQuery as unknown as InternalQuery)._query.path.canonicalString()
+    throw new Error(`Query for '${queryPath}' was not properly memoized using useMemoFirebase`);
   }
   return { data, isLoading, error };
 }
