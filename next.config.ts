37c2c1be15e81581c677259ab2a9dce3fa35ac00
Jerico Ada <mephistojeric.mcmxcii@gmail.com>
2025-11-16 04:31:56 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.stockcake.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
