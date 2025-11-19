178d5b017cb3771dbd7065d17ad0367a703cf28f
Jerico Ada <mephistojeric.mcmxcii@gmail.com>
2025-11-19 16:31:19 +0000

    overwrite the storagebucket, use this "almeriasdb.firebasestorage.app"

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,7 +6,7 @@ export const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "almeriasdb.firebaseapp.com",
   projectId: "almeriasdb",
-  storageBucket: "almeriasdb.appspot.com",
+  storageBucket: "almeriasdb.firebasestorage.app",
   messagingSenderId: "927407881844",
   appId: "1:927407881844:web:2912e3362b8c004ba29b21",
   measurementId: "G-Z7S88MJK95"
