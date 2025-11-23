18b8b6a7b39946ff4b89a1a47231509d4090eb13
Jerico Ada <mephistojeric.mcmxcii@gmail.com>
2025-11-23 04:26:25 +0000

    can you make the following Unit Quantity Allocated Amount Actual Amount

--- src/app/(app)/management/po/[poId]/page.tsx
+++ src/app/(app)/management/po/[poId]/page.tsx
@@ -137,29 +137,29 @@ export default function PoDetailsPage() {
                     <Table>
                         <TableHeader>
                             <TableRow>
-                                <TableHead>Item Name</TableHead>
-                                <TableHead>Unit</TableHead>
-                                <TableHead className="text-right">Quantity</TableHead>
-                                <TableHead className="text-right">Allocated Amount</TableHead>
-                                <TableHead className="text-right">Actual Amount</TableHead>
-                                <TableHead className="text-right">Total Allocation</TableHead>
-                                <TableHead className="text-right">Total Actual Cost</TableHead>
+                                <TableHead className="w-[20%]">Item Name</TableHead>
+                                <TableHead className="w-[5%]">Unit</TableHead>
+                                <TableHead className="text-right w-[5%]">Quantity</TableHead>
+                                <TableHead className="text-right w-[10%]">Allocated Amount</TableHead>
+                                <TableHead className="text-right w-[10%]">Actual Amount</TableHead>
+                                <TableHead className="text-right w-[10%]">Total Allocation</TableHead>
+                                <TableHead className="text-right w-[10%]">Total Actual Cost</TableHead>
                                 <TableHead>Description</TableHead>
-                                {user?.role === 'admin' && <TableHead className="text-right">Actions</TableHead>}
+                                {user?.role === 'admin' && <TableHead className="text-right w-[5%]">Actions</TableHead>}
                             </TableRow>
                         </TableHeader>
                         <TableBody>
                             {isLoading ? (
                                 Array.from({ length: 3 }).map((_, i) => (
                                     <TableRow key={i}>
-                                        <TableCell><Skeleton className="h-5 w-32" /></TableCell>
-                                        <TableCell><Skeleton className="h-5 w-16" /></TableCell>
-                                        <TableCell><Skeleton className="h-5 w-16 ml-auto" /></TableCell>
-                                        <TableCell><Skeleton className="h-5 w-24 ml-auto" /></TableCell>
-                                        <TableCell><Skeleton className="h-5 w-24 ml-auto" /></TableCell>
-                                        <TableCell><Skeleton className="h-5 w-24 ml-auto" /></TableCell>
-                                        <TableCell><Skeleton className="h-5 w-24 ml-auto" /></TableCell>
-                                        <TableCell><Skeleton className="h-5 w-24" /></TableCell>
+                                        <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                                        <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                                        <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                                        <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                                        <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                                        <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                                        <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                                        <TableCell><Skeleton className="h-5 w-full" /></TableCell>
                                         {user?.role === 'admin' && <TableCell><Skeleton className="h-8 w-8 ml-auto" /></TableCell>}
                                     </TableRow>
                                 ))
@@ -248,3 +248,5 @@ export default function PoDetailsPage() {
         </div>
     );
 }
+
+    
