c5bbb06426cb7a6d6feb2f6dea7edb96fc6eeaef
Jerico Ada <mephistojeric.mcmxcii@gmail.com>
2025-11-25 03:33:19 +0000

    make the gold crack style in loading to be more in screen like occupy th

--- src/components/layout/page-loader.tsx
+++ src/components/layout/page-loader.tsx
@@ -6,23 +6,25 @@ import { usePathname, useSearchParams } from 'next/navigation';
 import { cn } from '@/lib/utils';
 
 const KintsugiLoader = () => (
-    <svg width="150" height="150" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" className="kintsugi-loader">
+    <svg width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" className="kintsugi-loader">
         <style>{`
             .kintsugi-loader .crack {
                 stroke: hsl(var(--primary));
-                stroke-width: 0.75;
+                stroke-width: 0.5;
                 fill: none;
                 stroke-linecap: round;
-                stroke-dasharray: 200;
-                stroke-dashoffset: 200;
+                stroke-dasharray: 400;
+                stroke-dashoffset: 400;
                 animation: draw 2.5s ease-in-out forwards;
             }
             .kintsugi-loader .crack-1 { animation-delay: 0s; }
-            .kintsugi-loader .crack-2 { animation-delay: 0.2s; }
-            .kintsugi-loader .crack-3 { animation-delay: 0.4s; }
-            .kintsugi-loader .crack-4 { animation-delay: 0.6s; }
-            .kintsugi-loader .crack-5 { animation-delay: 0.8s; }
-            .kintsugi-loader .crack-6 { animation-delay: 1s; }
+            .kintsugi-loader .crack-2 { animation-delay: 0.1s; }
+            .kintsugi-loader .crack-3 { animation-delay: 0.2s; }
+            .kintsugi-loader .crack-4 { animation-delay: 0.3s; }
+            .kintsugi-loader .crack-5 { animation-delay: 0.4s; }
+            .kintsugi-loader .crack-6 { animation-delay: 0.5s; }
+            .kintsugi-loader .crack-7 { animation-delay: 0.6s; }
+            .kintsugi-loader .crack-8 { animation-delay: 0.7s; }
 
             @keyframes draw {
                 to {
@@ -31,12 +33,19 @@ const KintsugiLoader = () => (
             }
         `}</style>
         <g>
+            {/* Main vertical and horizontal cracks */}
             <path className="crack crack-1" d="M50 0 V 100" />
-            <path className="crack crack-2" d="M50 50 L 15 20" />
-            <path className="crack crack-3" d="M15 20 L 0 35" />
-            <path className="crack crack-4" d="M50 50 L 90 70" />
-            <path className="crack crack-5" d="M90 70 L 100 60" />
-            <path className="crack crack-6" d="M50 25 L 85 10" />
+            <path className="crack crack-2" d="M0 50 H 100" />
+            
+            {/* Cracks from center to corners */}
+            <path className="crack crack-3" d="M50 50 L 0 0" />
+            <path className="crack crack-4" d="M50 50 L 100 0" />
+            <path className="crack crack-5" d="M50 50 L 0 100" />
+            <path className="crack crack-6" d="M50 50 L 100 100" />
+
+            {/* Additional branching cracks */}
+            <path className="crack crack-7" d="M25 25 L 10 40" />
+            <path className="crack crack-8" d="M75 75 L 90 60" />
         </g>
     </svg>
 );
@@ -54,11 +63,11 @@ export function PageLoader() {
 
     const timer = setTimeout(() => {
         setIsPageLoading(false);
-    }, 800); // Increased duration for animation
+    }, 1200); // Increased duration for animation
 
     const fadeOutTimer = setTimeout(() => {
         setIsVisible(false);
-    }, 1100); // Delay fade out
+    }, 1500); // Delay fade out
 
     return () => {
         clearTimeout(timer);
