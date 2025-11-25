04c96f5b28e8ea28e5e82f076868fc7a2df39fa4
Jerico Ada <mephistojeric.mcmxcii@gmail.com>
2025-11-25 15:52:32 +0000

    increase the size of trigger button in the navbar on move it a little le

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
   
   return (
     <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b-2 border-b-foreground/20 bg-primary text-primary-foreground px-4 md:px-6 w-full">
-      <SidebarTrigger className="hidden md:flex" />
+      <SidebarTrigger className="hidden md:flex h-10 w-10 -ml-2" />
       <div className="flex-1">
         <h1 className="text-lg font-semibold md:hidden">
             {greeting}, {user?.displayName?.split(' ')[0]}!
