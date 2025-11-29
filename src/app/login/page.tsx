13cf35ad0222b4629aec51f741f3d76bb61136e7
Jerico Ada <mephistojeric.mcmxcii@gmail.com>
2025-11-29 03:59:54 +0000

    not cool, maybe light gray..

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -35,7 +35,7 @@ export default function LoginPage() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-black p-4">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
       <div className="w-full max-w-sm">
         <LoginHeader />
         <Tabs defaultValue="login" className="w-full">
