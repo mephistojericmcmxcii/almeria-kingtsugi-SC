a7a87ace411c966f1015224dd9055c3d7cf32ed9
Jerico Ada <mephistojeric.mcmxcii@gmail.com>
2025-12-01 13:33:40 +0000

    move to the right a little

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -28,12 +28,14 @@ const Header = () => {
   
   return (
     <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b-2 border-b-foreground/20 bg-primary px-4 md:px-6 w-full">
-      {/* Desktop sidebar trigger (visible when collapsed) */}
-      <SidebarTrigger className={cn("hidden h-7 w-7 -ml-2", !open && "md:flex")} />
-      
-      {/* Mobile sidebar trigger */}
-      <div className="md:hidden">
-         <SidebarTrigger />
+      <div className="flex items-center gap-4">
+          {/* Desktop sidebar trigger (visible when collapsed) */}
+          <SidebarTrigger className={cn("hidden h-7 w-7 -ml-2", !open && "md:flex")} />
+          
+          {/* Mobile sidebar trigger */}
+          <div className="md:hidden">
+            <SidebarTrigger />
+          </div>
       </div>
       
       <div className="flex-1">
